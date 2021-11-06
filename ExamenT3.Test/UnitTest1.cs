0043eca9b40698a54fe31c6ddcff4241c909ac48
Yan Medardo Minchan <yanmi@DESKTOP-B7A077C>
2021-11-05 21:02:25 -0500

    4

---
+++ ExamenT3.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace ExamenT3.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
